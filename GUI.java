6a32eeee44b2a4355b1ca3d95b5c47156e9a9044
Malte <maltefischer2011@outlook.de>
2023-05-12 23:30:48 +0200

    Fenster

--- GUI.java
+++ GUI.java
@@ -9,6 +9,7 @@ import java.awt.*;
 public class GUI extends JFrame {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private JLabel lblWirtschaftsleistung, lblModernisierungsgrad, lblLebensqualität, lblBildung;
+    private JLabel lblWirtschaftsleistungStand, lblModernisierungsgradStand,lblLebensqualitätStand, lblBildungStand;
     
     private JButton btWirtschaftHoch, btWirtschaftRunter, btModernHoch, btModernRunter, btLebenHoch, btLebenRunter, btBildungHoch, btBildungRunter;
 
@@ -17,54 +18,53 @@ public class GUI extends JFrame {
      * Konstruktor für Objekte der Klasse GUI
      */
     public GUI() {
-        setTitle("Ökolopoly");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(4, 3, 10, 10));
+        setTitle("Werte zuweisen");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//das Programm wird beendet wenn auf X geklickt wird
+        setLayout(new GridLayout(4, 4, 10, 10));
 
         lblWirtschaftsleistung = new JLabel("Wirtschaftsleistung: ");
+        lblWirtschaftsleistungStand = new JLabel("BEISPIEL");
         btWirtschaftHoch = new JButton("+");
         btWirtschaftRunter = new JButton("-");
 
         lblModernisierungsgrad = new JLabel("Modernisierungsgrad: ");
+        lblModernisierungsgradStand = new JLabel("BEISPIEL");
         btModernHoch = new JButton("+");
         btModernRunter = new JButton("-");
 
         lblLebensqualität = new JLabel("Lebensqualität: ");
+        lblLebensqualitätStand = new JLabel("BEISPIEL");
         btLebenHoch = new JButton("+");
         btLebenRunter = new JButton("-");
 
         lblBildung = new JLabel("Bildung: ");
+        lblBildungStand = new JLabel("BEISPIEL");
         btBildungHoch = new JButton("+");
         btBildungRunter = new JButton("-");
 
         add(lblWirtschaftsleistung);
+        add(lblWirtschaftsleistungStand);
         add(btWirtschaftHoch);
         add(btWirtschaftRunter);
 
         add(lblModernisierungsgrad);
+        add(lblModernisierungsgradStand);
         add(btModernHoch);
         add(btModernRunter);
 
         add(lblLebensqualität);
+        add(lblLebensqualitätStand);
         add(btLebenHoch);
         add(btLebenRunter);
 
         add(lblBildung);
+        add(lblBildungStand);
         add(btBildungHoch);
         add(btBildungRunter);
-        //Passt das Fenster auf die notwendige Größe an 
-        pack();
+         
+        pack();//Passt das Fenster auf die notwendige Größe an 
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public static void main(String[] args) {
-        new GUI();
-    }
 }
