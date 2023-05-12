49852867edfbdde884127a7bd9240ce1fb9639c6
Sven Vazquez de Lara Kallas <sven_vlk@yahoo.com>
2023-05-12 17:41:54 +0200

    Grober Ablauf hinzugefügt (siehe Main)

--- Main.java
+++ Main.java
@@ -8,26 +8,38 @@
 public class Main
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
 
-    /**
-     * Konstruktor für Objekte der Klasse Main
-     */
-    public Main()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    //Main Funktion
+    public static void main(String[] args) {
+        
+        
+        /*
+         * Grober Ablauf
+         * -------------
+         * 
+         * 1) Start des Spiels
+         *   ->Einlesen .sim Datei
+         *   -> Startwerte einlesen
+         *   -> Anzahl Runden festlegen
+         *   
+         *   [wiedehole 2 & 3 so oft wie es Runden gibt]
+         *   
+         * 2) Spieler Input
+         *   -> Anzeigen der Startwerte
+         *   -> Investiere Staatsvermögen (erstmalig)
+         *   
+         * 3) Berechnung
+         *   -> Größen der Sektoren wirken aufeinander in festgelegter Reihenfolge
+         *   -> prüfen, ob im Wertebereich (Abbruchsbedingung)
+         *   -> Simuilationserfolg ermitteln(pro Runde)
+         *   
+         *   [wiedehole 2 & 3 so oft wie es Runden gibt]
+         *   
+         * 4) Ende
+         *   -> speichern der Zwischenwerte/Simulationserfolge in .res Datei
+         *   -> Graph einblenden
+         *   -> (Highscore anzeigen)
+         *   -> Neues Spiel starten (reset aller Werte, Möglichkeit einlesen neuer Datei)
+         */
     }
 }
---
+++ Sektor.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Sektor here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sektor
+{
+    // instance variables - replace the example below with your own
+    private int[] wertebereich; //Akzeptierte Werte laut Aufgabenstellung
+    private String wird_beeinflusst_von; //Sektor, der Einfluss auf diese Kenngröße hat
+    private String beeinflusst;
+    private int wert; //Aktueller Wert
+    
+
+    /**
+     * Constructor for objects of class Sektor
+     */
+    public Sektor()
+    {
+        //TEST
+        this.wertebereich = new int[1];
+    }
+}
