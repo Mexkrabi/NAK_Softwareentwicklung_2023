f31d3dcfbb9a065f8ddae38eb4af78815b572d81
Sven Vazquez de Lara Kallas <sven_vlk@yahoo.com>
2023-05-14 00:26:57 +0200

    small correction

--- Main.java
+++ Main.java
@@ -21,8 +21,6 @@ public class Main
      */
     public static void main(String[] args) {
         
-        //Sneaky Kommentar hehehehehehe
-        
         //#  Grober Ablauf
         //#  -------------
         /* 0) Menü
