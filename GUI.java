2574ed85b424b59a93a7521bed4b04f401e6a3f7
MalteF01 <55228672+MalteF01@users.noreply.github.com>
2023-05-13 19:07:54 +0200

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
 /**
  * Beschreiben Sie hier die Klasse GUI.
  * 
@@ -12,26 +11,29 @@ import java.awt.event.*;
 public class GUI extends JFrame implements ActionListener {
     // Instanzvariablen 
     private JFrame fenster;
+    private JPanel startBildschirm, wertezuweisen, auswahlDatei;
+    private JComboBox<String> cbDateien;
+    private String strSpielstand, strAuswahl;
     private String spielstand; //# <-- durch ENUM ersetzen in der Main
     private JLabel lblwilkommen;
-    private JButton btStart, btEnde;
+    private JButton btStart, btEnde, btAuswahlBestätigen;
     private JLabel lblWirtschaftsleistung, lblModernisierungsgrad, lblLebensqualität, lblBildung;
     private JLabel lblWirtschaftsleistungStand, lblModernisierungsgradStand,lblLebensqualitätStand, lblBildungStand;
     private JButton btWirtschaftHoch, btWirtschaftRunter, btModernHoch, btModernRunter, btLebenHoch, btLebenRunter, btBildungHoch, btBildungRunter;
-
-
+    
     /**
      * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
      * 
      * @param  y    (Beschreibung des Parameters)
-     * @return        (Beschreibung des Rückgabewertes)
      */
     public void spielstandänderung()
     {
-        switch (spielstand) {
+        switch (strSpielstand) {
             case "Start":
                 startBildschirm();
                 break;
+            case "Auswahl" :
+                dateiAuswal();
             case "Wertezuweisen":
                 wertezuweisung();
 
@@ -48,33 +50,66 @@ public class GUI extends JFrame implements ActionListener {
     {
         // tragen Sie hier den Code ein
         fenster = new JFrame("Start-End Frame");
-        JPanel panel = new JPanel();
+        startBildschirm = new JPanel();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        panel.setLayout(new GridLayout(3, 1, 30, 30));
-        
+        startBildschirm.setLayout(new GridLayout(3, 1, 30, 30));
 
         // Label & Button erstellen und hinzufügen
         lblwilkommen = new JLabel("Willkommen!");
         btStart = new JButton("Start");
         btEnde = new JButton("Ende");
-        
-        
+
         btStart.addActionListener(this);
         btEnde.addActionListener(this);
-        
-        //Hinzufügen der Button und des Label
-        panel.add(lblwilkommen);
-        panel.add(btStart);
-        panel.add(btEnde);
 
-        fenster.add(panel);
-        
+        //Hinzufügen der Button und des Label
+        startBildschirm.add(lblwilkommen);
+        startBildschirm.add(btStart);
+        startBildschirm.add(btEnde);
+
+        fenster.add(startBildschirm);
+
         //  Größe des Frames festlegen
-        panel.setSize(300, 300);
+        startBildschirm.setSize(300, 300);
         fenster.setSize(300,300);
         setLocationRelativeTo(null);
         fenster.setVisible(true);
-        panel.setVisible(true);
+        startBildschirm.setVisible(true);
+    }
+    
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y	(Beschreibung des Parameters)
+     * @return		(Beschreibung des Rückgabewertes)
+     */
+    public void dateiAuswal()
+    {
+        // tragen Sie hier den Code ein
+        fenster = new JFrame("Start-End Frame");
+        fenster.setTitle("Werte zuweisen");
+        auswahlDatei = new JPanel();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);//das Programm wird beendet wenn auf X geklickt wird     
+        auswahlDatei.setLayout(new GridLayout(3, 2, 40, 40));
+        
+        JLabel lblAuswahl = new JLabel("Wählen Sie das zu Spielende Land aus");
+        btAuswahlBestätigen = new JButton("Auswahl bestätigen");
+        cbDateien = new JComboBox<String>();
+    
+        cbDateien.addItem("Auswahl");
+        cbDateien.addItem("Hier die Dateinamen einfügen");
+        
+        btAuswahlBestätigen.addActionListener(this);
+        
+        auswahlDatei.add(lblAuswahl);
+        auswahlDatei.add(cbDateien); 
+        auswahlDatei.add(btAuswahlBestätigen);
+        fenster.add(auswahlDatei);
+        
+        fenster.setVisible(true);
+        auswahlDatei.setVisible(true);
+        fenster.setSize(300,300);;//Passt das Fenster auf die notwendige Größe an 
+        fenster.setLocationRelativeTo(null);
     }
 
     /**
@@ -85,11 +120,11 @@ public class GUI extends JFrame implements ActionListener {
      */
     private void wertezuweisung()
     {
-        fenster = new JFrame("Werte zuweisen");
-        
-        JPanel panel = new JPanel();
+        fenster.setTitle("Werte zuweisen");
+
+        wertezuweisen = new JPanel();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//das Programm wird beendet wenn auf X geklickt wird
-        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        wertezuweisen.setLayout(new GridLayout(4, 4, 10, 10));
 
         lblWirtschaftsleistung = new JLabel("Wirtschaftsleistung: ");
         lblWirtschaftsleistungStand = new JLabel("BEISPIEL");
@@ -106,7 +141,7 @@ public class GUI extends JFrame implements ActionListener {
 
         btModernHoch.addActionListener(this);
         btModernRunter.addActionListener(this);
-        
+
         lblLebensqualität = new JLabel("Lebensqualität: ");
         lblLebensqualitätStand = new JLabel("BEISPIEL");
         btLebenHoch = new JButton("+");
@@ -114,89 +149,94 @@ public class GUI extends JFrame implements ActionListener {
 
         btLebenHoch.addActionListener(this);
         btLebenRunter.addActionListener(this);
-        
+
         lblBildung = new JLabel("Bildung: ");
         lblBildungStand = new JLabel("BEISPIEL");
         btBildungHoch = new JButton("+");
         btBildungRunter = new JButton("-");
-        
+
         btBildungHoch.addActionListener(this);
         btBildungRunter.addActionListener(this);
-        
-        fenster.add(panel);
-        
-        panel.add(lblWirtschaftsleistung);
-        panel.add(lblWirtschaftsleistungStand);
-        panel.add(btWirtschaftHoch);
-        panel.add(btWirtschaftRunter);
 
-        panel.add(lblModernisierungsgrad);
-        panel.add(lblModernisierungsgradStand);
-        panel.add(btModernHoch);
-        panel.add(btModernRunter);
+        fenster.add(wertezuweisen);
 
-        panel.add(lblLebensqualität);
-        panel.add(lblLebensqualitätStand);
-        panel.add(btLebenHoch);
-        panel.add(btLebenRunter);
+        wertezuweisen.add(lblWirtschaftsleistung);
+        wertezuweisen.add(lblWirtschaftsleistungStand);
+        wertezuweisen.add(btWirtschaftHoch);
+        wertezuweisen.add(btWirtschaftRunter);
 
-        panel.add(lblBildung);
-        panel.add(lblBildungStand);
-        panel.add(btBildungHoch);
-        panel.add(btBildungRunter);
+        wertezuweisen.add(lblModernisierungsgrad);
+        wertezuweisen.add(lblModernisierungsgradStand);
+        wertezuweisen.add(btModernHoch);
+        wertezuweisen.add(btModernRunter);
+
+        wertezuweisen.add(lblLebensqualität);
+        wertezuweisen.add(lblLebensqualitätStand);
+        wertezuweisen.add(btLebenHoch);
+        wertezuweisen.add(btLebenRunter);
+
+        wertezuweisen.add(lblBildung);
+        wertezuweisen.add(lblBildungStand);
+        wertezuweisen.add(btBildungHoch);
+        wertezuweisen.add(btBildungRunter);
 
         fenster.setVisible(true);
-        panel.setVisible(true);
+        wertezuweisen.setVisible(true);
         fenster.pack();//Passt das Fenster auf die notwendige Größe an 
         fenster.setLocationRelativeTo(null);
     }
-    
+
     //Getter & Setter
     public String getSpielstand() 
     {
-        return this.spielstand;
+        return this.strSpielstand;
     }
+
     public void setSpielstand(String neuerSpielstand) 
     {
-        this.spielstand = neuerSpielstand;
+        this.strSpielstand = neuerSpielstand;
         //Prüfung hinzufügen
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btStart) {
             // Aktion für Button 1
             System.out.println("Start wurde geklickt.");
-            setSpielstand("Auslesen");
-            fenster.dispose();
+            setSpielstand("Auswahl");
+            startBildschirm.setVisible(false);
         } else if (e.getSource() == btEnde) {
             // Aktion für Button 2
             System.out.println("Ende wurde geklickt.");
             System.exit(0);
         } else if (e.getSource() == btModernHoch) {
-            
+
             System.out.println("Modernisierung um 1 Punkt hoch");
         } else if (e.getSource() == btModernRunter) {
-            
+
             System.out.println("Modernisierung um 1 Punkt runter");
-            } else if (e.getSource() == btWirtschaftHoch) {
-                
+        } else if (e.getSource() == btWirtschaftHoch) {
+
             System.out.println("Wirtschaftleistung um 1 Punkt hoch");
         } else if (e.getSource() == btWirtschaftRunter) {
-                
+
             System.out.println("Wirtschaftleistung um 1 Punkt runter");
         } else if (e.getSource() == btLebenHoch) {
-                
+
             System.out.println("Lebensqualität um 1 Punkt hoch");
         } else if (e.getSource() == btLebenRunter) {
-                
+
             System.out.println("Lebensqualität um 1 Punkt runter");
         } else if (e.getSource() == btBildungHoch) {
-                
+
             System.out.println("Bildung um 1 Punkt hoch");
         } else if (e.getSource() == btBildungRunter) {
-                
+
             System.out.println("Bildung um 1 Punkt runter");
-        }
+        } else if (e.getSource() == btAuswahlBestätigen) {
+
+            strAuswahl = (String) cbDateien.getSelectedItem();
+            System.out.println(strAuswahl);
         }
     }
+}
