0020a4d2990fb7b984e59ed9668101a8e0e3e5e0
Livia <livia@schoenholtz.de>
2023-05-28 12:06:00 +0200

    Methode kommentiert

--- DateiLeser.java
+++ DateiLeser.java
@@ -96,7 +96,7 @@ public class DateiLeser
             while ((line = br.readLine()) != null) 
             {
                 String ausgelesenerWert;
-                // Suche nach dem Suchwort vvvvvvvvvvvvvvvvvvvvvvvvvvvv
+                // Suche nach dem Suchwort, start
                 if (line.startsWith(suchwort)) 
                 {
                     System.out.println("Eingabezeile: " + line.substring(0));
@@ -104,7 +104,7 @@ public class DateiLeser
                     System.out.println("Ausgelesener Wert: " + ausgelesenerWert);
                     return ausgelesenerWert;
                 }
-                // Suche nach dem Suchwort ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                // Suche nach dem Suchwort, ende
             }
         } catch (IOException e) 
         {
@@ -113,13 +113,14 @@ public class DateiLeser
         return "Wert konnte nicht gefunden werden.";
     }
 
-    //#TODO @Livia
+    
     /**
-     * [BESCHREIBUNG]
+     * Die Methode erstellt ein HashSet mit den Namen der SIM Datein im Projektordner
+     * Sie prüft, dass es sich um eine Datei handelt und diese eine .sim Endung hat
      * 
-     * Quelle zur Dokumentation: [LINK]
+     * Quelle zur Dokumentation: https://docs.oracle.com/javase/8/docs/api/java/util/HashSet.html
      * 
-     * [RETURN]
+     * @Return: HashSet mit SIM Dateinamen ohne Endung
      */
     public Set<String> dateienFinden(){
         Set<String> dateien = new HashSet<String>();
@@ -138,34 +139,33 @@ public class DateiLeser
         return this.dieserOrdner;
     }
     
-
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////        REFERENZ        ////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public static void main(String[] args) 
-    {
-        String currentPath = System.getProperty("user.dir");
-        System.out.println("Pfad des aktuellen Projektordners: " + currentPath + "/beispielland.sim");
-        String simFilePath = currentPath + "/beispielland.sim";
-        try (BufferedReader br = new BufferedReader(new FileReader(simFilePath))) 
-        {
-            String line;
-            while ((line = br.readLine()) != null) 
-            {
-                // Hier kannst du die Zeilen der SIM-Datei auf Werte prüfen und in Variablen speichern
-                // Beispiel: Wenn die Zeile mit "name=" beginnt, speichere den Namen in der Variable "name"
-                // BEISPIELCODE vvvvvvvvvvvvvvvvvvvvvvvvvvvv
-                if (line.startsWith("name=")) 
-                {
-                    String name = line.substring(5);
-                    System.out.println("Name: " + name);
-                }
-                //BEISPIELCODE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-                System.out.println(line.substring(0));
-            }
-        } catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
-    }
+    // public static void main(String[] args) 
+    // {
+        // String currentPath = System.getProperty("user.dir");
+        // System.out.println("Pfad des aktuellen Projektordners: " + currentPath + "/beispielland.sim");
+        // String simFilePath = currentPath + "/beispielland.sim";
+        // try (BufferedReader br = new BufferedReader(new FileReader(simFilePath))) 
+        // {
+            // String line;
+            // while ((line = br.readLine()) != null) 
+            // {
+                // // Hier kannst du die Zeilen der SIM-Datei auf Werte prüfen und in Variablen speichern
+                // // Beispiel: Wenn die Zeile mit "name=" beginnt, speichere den Namen in der Variable "name"
+                // // BEISPIELCODE vvvvvvvvvvvvvvvvvvvvvvvvvvvv
+                // if (line.startsWith("name=")) 
+                // {
+                    // String name = line.substring(5);
+                    // System.out.println("Name: " + name);
+                // }
+                // //BEISPIELCODE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                // System.out.println(line.substring(0));
+            // }
+        // } catch (IOException e) 
+        // {
+            // e.printStackTrace();
+        // }
+    // }
 }
---
+++ res-Dateien/Livia-Ergebnis_20230523_211706.res
@@ -0,0 +1,61 @@
+Simulationserfolg über die Runden:
+
+Simulationserfolg in Runde 1 = 58
+Simulationserfolg in Runde 2 = 56
+Simulationserfolg in Runde 3 = 42
+Simulationserfolg in Runde 4 = 49
+
+Bevölkerungsgröße in Runde 1 = 22
+Bevölkerungsgröße in Runde 2 = 18
+Bevölkerungsgröße in Runde 3 = 15
+Bevölkerungsgröße in Runde 4 = 15
+
+Bevölkerungswachstum in Runde 1 = 8
+Bevölkerungswachstum in Runde 2 = 13
+Bevölkerungswachstum in Runde 3 = 14
+Bevölkerungswachstum in Runde 4 = 14
+
+Wirtschaftsleistung in Runde 1 = 21
+Wirtschaftsleistung in Runde 2 = 22
+Wirtschaftsleistung in Runde 3 = 24
+Wirtschaftsleistung in Runde 4 = 26
+
+Modernisierungsgrad in Runde 1 = 8
+Modernisierungsgrad in Runde 2 = 9
+Modernisierungsgrad in Runde 3 = 10
+Modernisierungsgrad in Runde 4 = 13
+
+Politische Stabilität in Runde 1 = 7
+Politische Stabilität in Runde 2 = 8
+Politische Stabilität in Runde 3 = 7
+Politische Stabilität in Runde 4 = 7
+
+Umweltverschmutzung in Runde 1 = 18
+Umweltverschmutzung in Runde 2 = 19
+Umweltverschmutzung in Runde 3 = 25
+Umweltverschmutzung in Runde 4 = 37
+
+Lebensqualität in Runde 1 = 13
+Lebensqualität in Runde 2 = 12
+Lebensqualität in Runde 3 = 8
+Lebensqualität in Runde 4 = 13
+
+Bildung in Runde 1 = 7
+Bildung in Runde 2 = 19
+Bildung in Runde 3 = 29
+Bildung in Runde 4 = 29
+
+Staatsvermögen in Runde 1 = 12
+Staatsvermögen in Runde 2 = 12
+Staatsvermögen in Runde 3 = 11
+Staatsvermögen in Runde 4 = 3
+
+Bevölkerungswachstumsfaktor in Runde 1 = 2
+Bevölkerungswachstumsfaktor in Runde 2 = 2
+Bevölkerungswachstumsfaktor in Runde 3 = 1
+Bevölkerungswachstumsfaktor in Runde 4 = 1
+
+Versorgungslage in Runde 1 = 1
+Versorgungslage in Runde 2 = 1
+Versorgungslage in Runde 3 = 1
+Versorgungslage in Runde 4 = 1
