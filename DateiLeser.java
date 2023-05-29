b790a11045cc65a7979d8be0be72da3d73d67446
Livia <livia@schoenholtz.de>
2023-05-29 13:17:22 +0200

    Beispielland nachgebaut geht nicht und ich weiß noch nicht warum

--- DateiLeser.java
+++ DateiLeser.java
@@ -137,34 +137,4 @@ public class DateiLeser
     {
         return this.dieserOrdner;
     }
-    
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////        REFERENZ        ////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // public static void main(String[] args) 
-    // {
-        // String currentPath = System.getProperty("user.dir");
-        // System.out.println("Pfad des aktuellen Projektordners: " + currentPath + "/beispielland.sim");
-        // String simFilePath = currentPath + "/beispielland.sim";
-        // try (BufferedReader br = new BufferedReader(new FileReader(simFilePath))) 
-        // {
-            // String line;
-            // while ((line = br.readLine()) != null) 
-            // {
-                // // Hier kannst du die Zeilen der SIM-Datei auf Werte prüfen und in Variablen speichern
-                // // Beispiel: Wenn die Zeile mit "name=" beginnt, speichere den Namen in der Variable "name"
-                // // BEISPIELCODE vvvvvvvvvvvvvvvvvvvvvvvvvvvv
-                // if (line.startsWith("name=")) 
-                // {
-                    // String name = line.substring(5);
-                    // System.out.println("Name: " + name);
-                // }
-                // //BEISPIELCODE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-                // System.out.println(line.substring(0));
-            // }
-        // } catch (IOException e) 
-        // {
-            // e.printStackTrace();
-        // }
-    // }
 }
